c388064c59ce9bec70bf40f99ce6e7d8a227f21b
wizard <pranjalpatel9838@gmail.com>
2023-08-31 19:23:54 +0530

    Added example script

---
+++ script.lua
@@ -0,0 +1,7 @@
+mm.sleep(2000);
+mm.key_seq("{+META} {-META}")
+mm.sleep(100);
+mm.key_seq("alacritty{+RETURN}{-RETURN}")
+mm.sleep(1000);
+mm.type_text("neofetch");
+mm.key_seq("{+RETURN}{-RETURN}")
